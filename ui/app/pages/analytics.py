fd2c03e5760a0fc2f8a42cb2e7766e7df74a983a
Jose Roberto <joserobertomi@outlook.com>
2025-10-16 00:59:37 -0300

    feat: add analytics page with data visualization options
    - Introduced a new analytics page in the Streamlit app, allowing users to select between a real-time demo and stored analytics.
    - Implemented functions to display data from a CSV file, enhancing the application's data visualization capabilities.
    - Added a sample CSV file containing detection data for testing and demonstration purposes.

--- ui/app/pages/analytics.py
+++ ui/app/pages/analytics.py
@@ -1,10 +1,26 @@
 import streamlit as st
+import pandas as pd
 
+def real_demo():
+    st.write("Real demo")
+    st.write("This is a real demo of the analytics - based in data requested and api responses")
+
+def stored_analytics():
+    st.write("Stored analytics")
+    st.write("This is a stored analytics - based in data stored in a csv file")
+    
+    df = pd.read_csv("data/detection-example.csv")
+    st.dataframe(df)
+    
 def main():
     st.title("🚀 FastCV Beta Frontend - Analytics")
     st.write("Welcome to the FastCV application - Analytics!")
     st.divider()
-    st.warning("This page is under construction")
+    metric = st.sidebar.selectbox("Select a format for analytics", ["Real demo", "Stored analytics"])
+    if metric == "Real demo":
+        real_demo()
+    elif metric == "Stored analytics":
+        stored_analytics()
     
 if __name__ == "__main__":
     main()
